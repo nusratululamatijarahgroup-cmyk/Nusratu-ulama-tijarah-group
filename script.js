39ffb8e21517c7d538e9ffb382642a4c8074f27e
nusratululamatijarahgroup-cmyk <nusratululamatijarahgroup@gmail.com>
2026-05-17 19:32:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function openMenu() {
-  document.getElementById('sidebar').classList.add('active');
+function openMenu(){
+document.getElementById('sidebar').classList.add('active');
 }
 
-function closeMenu() {
-  document.getElementById('sidebar').classList.remove('active');
+function closeMenu(){
+document.getElementById('sidebar').classList.remove('active');
 }
+
+const date = new Date();
+
+const days = [
+"রবিবার",
+"সোমবার",
+"মঙ্গলবার",
+"বুধবার",
+"বৃহস্পতিবার",
+"শুক্রবার",
+"শনিবার"
+];
+
+document.getElementById("currentDate").innerText =
+date.toLocaleDateString("bn-BD");
+
+document.getElementById("currentDay").innerText =
+days[date.getDay()];
